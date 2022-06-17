f13de2ebda347962ec4e76be0a894f1aa1fd66e3
buncho315 <mzh.hrtk125@gmail.com>
2022-06-17 07:34:35 +0000

    Initial Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_15_010947) do
+ActiveRecord::Schema.define(version: 2022_06_15_131609) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,9 +40,20 @@ ActiveRecord::Schema.define(version: 2022_06_15_010947) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "post_images", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "shop_name"
+    t.string "image"
+    t.string "caption"
+    t.string "user_id"
   end
 
   create_table "users", force: :cascade do |t|
